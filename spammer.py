aa30756088c3824c089b77406276d15870862581
Aidenwjt <55963813+Aidenwjt@users.noreply.github.com>
2022-05-13 21:32:20 -0600

    changes

--- spammer.py
+++ spammer.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 browser = webdriver.Firefox()
 browser.get('http://noah.binaryfox.ca/feedback.php')
+
+elem = browser.find_element_by_name('feedback')
+elem.send_keys('You suck at league of legends')
+
+search_btn = browser.find_element(By.NAME, 'submit')
+webdriver.ActionChains(browser).double_click(search_btn).perform()
